2f029ff9eb4c198d857c66fc3ec50c8b8af9ce04
김 미성 <rlaaltjd46@jinhak.com>
2018-06-26 12:00:04 +0900

    1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,7 @@ export default class Contact extends Component {
     //그 부작용으로 리액트 컴포넌트가 리로딩될때 생성자 실행 안함 -> 생성자 수정 되면 새로고침 해줘야함
     super(props);
     this.state = {
+      selectedKey: -1,
       keyword: "",
       contactData: [
         {
@@ -29,6 +30,7 @@ export default class Contact extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   //handleChange는 this가 뭔지 모름 그러므로 위처럼 bind 해줘야함
   handleChange(e) {
@@ -38,14 +40,32 @@ export default class Contact extends Component {
     });
   }
 
+  handleClick(key) {
+    this.setState({
+      selectedKey: key
+    });
+
+    console.log(key, "is selected");
+  }
+
   render() {
     const mapToComponents = data => {
       data.sort(); //오름차순 정렬하기 때문에 파라미터 생략가능
       data = data.filter(contact => {
         return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
       });
+      //컴포넌트에서는 onClick 같은 이벤트 적용안됨 네이티브 돔 이런 곳에 적용됨
       return data.map((contact, i) => {
-        return <ContactInfo contact={contact} key={i} />;
+        return (
+          <ContactInfo
+            contact={contact}
+            key={i}
+            //onClick props로 전달된다. 그래서 contactinfo에서 전달 받음
+            onClick={() => {
+              console.log("A");
+            }}
+          />
+        );
       });
     };
 
